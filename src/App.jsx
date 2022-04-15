7ffa208043b991b1fd57e5698565674694e70638
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2022-04-15 11:20:13 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="main">
+
+          <div className="header">
+            <h1>Todo App</h1>
+          </div>
+          <div className="body">
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
