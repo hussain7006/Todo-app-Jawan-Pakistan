f510f5ff5ae5ccde237e31251eba7ae25150d544
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2022-04-15 12:07:23 +0500

    Added delete attribute

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Icon from "react-crud-icons";
+import "../node_modules/react-crud-icons/dist/css/react-crud-icons.css";
+import { useEffect, useState } from 'react';
 
 function App() {
+  // let todo = ['asdf', 'dfg', 'zxvbxcv', 'zxcvzxcv'];
+
+  let [todos, setTodos] = useState(['asdf', 'dfg', 'zxvbxcv', 'zxcvzxcv'])
+
+  const delteHandler = (e) => {
+    const newTodos = [...todos];
+    newTodos.splice(e, 1);
+    setTodos(newTodos);
+  }
+    useEffect(()=>{
+
+  },[todos])
+
   return (
     <>
       <div className="container">
@@ -11,7 +26,30 @@ function App() {
             <h1>Todo App</h1>
           </div>
           <div className="body">
-
+            <div className="input">
+              <input type="text" />
+              <button>Add</button>
+            </div>
+            {todos.map((e, i) =>
+              <div className='data' key={i}>
+                <div className='todo'>{e}</div>
+                <div className='buttonsDiv'>
+                  <Icon
+                    name="edit"
+                    tooltip="Edit"
+                    theme="dark"
+                    size="medium"
+                  />
+                  <Icon
+                    name="delete"
+                    tooltip="Delte"
+                    theme="dark"
+                    size="medium"
+                    onClick={(i) => delteHandler(i)}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
