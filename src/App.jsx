07b1bf184f26ef869fa719c16a5e3cbd5ea8f5a5
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2022-04-18 11:50:54 +0500

    Functionality Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import { useEffect, useState } from 'react';
 function App() {
   // let todo = ['asdf', 'dfg', 'zxvbxcv', 'zxcvzxcv'];
 
-  let [todos, setTodos] = useState(['asdf', 'dfg', 'zxvbxcv', 'zxcvzxcv']);
+  let [todos, setTodos] = useState([]);
   let [newTodo, setnewTodo] = useState('');
+  let [updateTodo, setUpdateTodo] = useState('');
 
   const delteHandler = (e) => {
     const newTodos = [...todos];
@@ -15,13 +16,34 @@ function App() {
     setTodos(newTodos);
   }
 
-  const addNewTodo = (e) => {
+  const addNewTodo = () => {
     if (newTodo) {
-      setTodos([newTodo, ...todos])
-      setnewTodo('')
 
+      setTodos([{ todo: newTodo }, ...todos])
+      setnewTodo('')
     }
   }
+
+  const editHandler = (index) => {
+
+    todos[index].edit = true
+    setTodos([...todos])
+
+  }
+
+  const updateHandler = (index) => {
+
+    if (updateTodo) {
+      todos[index].todo = updateTodo;
+      todos[index].edit = false;
+      setUpdateTodo('')
+    } else {
+      todos[index].edit = false;
+      setTodos([...todos])
+    }
+
+  }
+
   useEffect(() => {
 
   }, [todos, newTodo])
@@ -34,29 +56,40 @@ function App() {
           <div className="header">
             <h1>Todo App</h1>
           </div>
-          <div className="body">
             <div className="input">
               <input type="text" value={newTodo} onChange={(e) => setnewTodo(e.target.value)} />
-              <button onClick={(e) => addNewTodo(e)}>Add</button>
+              <button onClick={addNewTodo}>Add</button>
             </div>
+          <div className="body">
             {todos.map((e, i) =>
+
               <div className='data' key={i}>
-                <div className='todo'>{e}</div>
-                <div className='buttonsDiv'>
-                  <Icon
-                    name="edit"
-                    tooltip="Edit"
-                    theme="dark"
-                    size="medium"
-                  />
-                  <Icon
-                    name="delete"
-                    tooltip="Delte"
-                    theme="dark"
-                    size="medium"
-                    onClick={(i) => delteHandler(i)}
-                  />
-                </div>
+                {!e.edit ? <>
+                  <div className='todo'>{e.todo}</div>
+                  <div className='buttonsDiv'>
+                    <Icon
+                      name="edit"
+                      tooltip="Edit"
+                      theme="dark"
+                      size="medium"
+                      onClick={() => editHandler(i)}
+                    />
+                    <Icon
+                      name="delete"
+                      tooltip="Delte"
+                      theme="dark"
+                      size="medium"
+                      onClick={(i) => delteHandler(i)}
+                    />
+                  </div>
+                </>
+                  :
+                  <div className='updateDiv'>
+                    <input type="text" onChange={(e) => setUpdateTodo(e.target.value)}
+                      value={updateTodo} />
+                    <button onClick={() => updateHandler(i)}>Update</button>
+                  </div>
+                }
               </div>
             )}
           </div>
